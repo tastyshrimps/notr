cf553e9d6f69ebbc937117911b82fc544817fc44
tastyshrimps <simon.huch@gmail.com>
2021-03-03 20:06:37 +0100

    create, edit fertig

--- _mysql/notr.sql
+++ _mysql/notr.sql
@@ -57,7 +57,7 @@ CREATE TABLE `notizen` (
   `deleted` tinyint(1) NOT NULL,
   `updated` datetime NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=327 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=336 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +66,7 @@ CREATE TABLE `notizen` (
 
 LOCK TABLES `notizen` WRITE;
 /*!40000 ALTER TABLE `notizen` DISABLE KEYS */;
-INSERT INTO `notizen` VALUES (326,'',0,NULL,NULL,0,'0000-00-00 00:00:00');
+INSERT INTO `notizen` VALUES (331,'asdffff',0,NULL,NULL,0,'0000-00-00 00:00:00'),(332,' asdf',0,NULL,NULL,0,'0000-00-00 00:00:00'),(333,'ff',0,NULL,NULL,0,'0000-00-00 00:00:00'),(335,'',0,NULL,NULL,0,'0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `notizen` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +121,7 @@ CREATE TABLE `notizen_text` (
 
 LOCK TABLES `notizen_text` WRITE;
 /*!40000 ALTER TABLE `notizen_text` DISABLE KEYS */;
-INSERT INTO `notizen_text` VALUES (326,274);
+INSERT INTO `notizen_text` VALUES (331,279),(332,280),(333,281),(335,283);
 /*!40000 ALTER TABLE `notizen_text` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -160,7 +160,7 @@ CREATE TABLE `text` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `section` text NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=275 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=284 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,7 +169,7 @@ CREATE TABLE `text` (
 
 LOCK TABLES `text` WRITE;
 /*!40000 ALTER TABLE `text` DISABLE KEYS */;
-INSERT INTO `text` VALUES (1,'Dienstag 16.2.2021'),(2,'Bestellung Tabletten für Oma'),(3,'Brot\r\nFisch \r\nGemüse'),(4,'Freitag 28.2.2021'),(5,'Lorem ipsum...'),(6,'Lorem ipsum...'),(7,'Simon ist toll'),(8,'Simon ist toll'),(9,'Simon ist toll'),(10,'Lorem iasdlgkjaölsdjfpsum...'),(11,'Lorem iasdlgkjaölsdjfpsum...'),(12,'asdf'),(13,'asdf'),(14,''),(15,''),(16,''),(17,'sg'),(18,''),(19,''),(20,'r'),(21,'c'),(22,''),(23,''),(24,''),(25,''),(26,''),(27,'asdf'),(28,'a'),(29,'ff'),(30,'fft'),(31,'fft'),(32,'g'),(33,''),(34,''),(35,''),(36,'f'),(37,'f'),(38,'fa'),(39,' fa '),(40,'  fa  '),(41,'   fa   '),(42,'    fa    '),(43,'     fa     '),(44,'      fa      '),(45,'       fa       '),(46,'        fa        '),(47,'         fa         '),(48,'          fa          '),(49,'fe'),(50,'fe'),(51,'fe'),(52,'nwtr'),(53,'nwtr'),(54,'saaa'),(55,'vv'),(56,''),(57,'headertest'),(58,'ffA'),(59,''),(60,'aafd'),(61,'tier'),(62,'wurst'),(63,'qq'),(64,'HET'),(65,''),(66,''),(67,''),(68,''),(69,''),(70,''),(71,''),(72,''),(73,''),(74,''),(75,''),(76,''),(77,''),(78,''),(79,''),(80,''),(81,''),(82,''),(83,''),(84,''),(85,''),(86,''),(87,''),(88,''),(89,''),(90,''),(91,''),(92,''),(93,''),(94,''),(95,''),(96,''),(97,''),(98,''),(99,''),(100,''),(101,''),(102,''),(103,''),(104,'grw'),(105,''),(106,''),(107,''),(108,''),(109,''),(110,''),(111,''),(112,''),(113,''),(114,''),(115,''),(116,''),(117,''),(118,''),(119,''),(120,''),(121,''),(122,''),(123,''),(124,''),(125,''),(126,''),(127,''),(128,''),(129,''),(130,''),(131,''),(132,''),(133,''),(134,''),(135,''),(136,''),(137,''),(138,''),(139,''),(140,''),(141,''),(142,''),(143,''),(144,''),(145,''),(146,''),(147,''),(148,''),(149,''),(150,''),(151,''),(152,''),(153,''),(154,''),(155,''),(156,''),(157,''),(158,''),(159,''),(160,''),(161,''),(162,''),(163,''),(164,'G$RW'),(165,''),(166,'RAA'),(167,'aww'),(168,'aebr'),(169,'65qw'),(170,'Ist ganz toll'),(171,''),(172,'mv '),(173,'mv '),(174,'mv '),(175,''),(176,''),(177,'WGVD'),(178,''),(179,''),(180,''),(181,''),(182,''),(183,''),(184,''),(185,'ff'),(186,'ff'),(187,'ff'),(188,'ff'),(189,'dA'),(190,'<br />\r\n<b>Warning</b>:  Undefined variable $text in <b>C:xampphtdocsgit\notredit.php</b> on line <b>52</b><br />\r\n'),(191,''),(192,'ASDF'),(193,'SGH'),(194,'SGH'),(195,'SGH'),(196,'SGH'),(197,'SGH'),(198,'SGH'),(199,'ADSF'),(200,'ADSF'),(201,'ADSF'),(202,'ADSF'),(203,'- Kurze Haare\r\n- Informatiker'),(204,''),(205,'asdf'),(206,''),(207,'asdf'),(208,''),(209,''),(210,'aegr'),(211,''),(212,''),(213,''),(214,''),(215,''),(216,'WEGF'),(217,'asdf'),(218,'aaa'),(219,'aaa'),(220,'awef'),(221,'aer'),(222,'Testtext1'),(223,'def'),(224,'aSFD'),(225,'ttt'),(226,'qaet'),(227,'z4'),(228,'aasdf'),(229,'asdf'),(230,'asdff'),(231,'adsf'),(232,'asdfasdfSE'),(233,'ooooo'),(234,'ooooo'),(235,'asdfasdfa'),(236,'rrrrr'),(237,'ppppp'),(238,'asdf'),(239,'asdf'),(240,'nein'),(241,'kkkkk'),(242,'FS'),(243,'WGE'),(244,'ffff'),(245,'ppp'),(246,'gg'),(247,'asdf'),(248,'ethat'),(249,'asdf'),(250,'asdf'),(251,''),(252,'asdf'),(253,'WGR'),(254,'RH<'),(255,'AGDFS'),(256,'fff'),(257,'ffff'),(258,'fff'),(259,'asdf'),(260,'dgdrgr'),(261,'asdf'),(262,'gR'),(263,'ggghtasdfasdasdf'),(264,'Butterhhh\r\nMehl'),(265,'Zucker\r\nMilch'),(266,'athreg'),(267,'fasdfasdfaaagfdtzksfdajd'),(268,'Schlimme Sache, das'),(269,'asdfasdfa'),(270,'asdfasdfasdfffff'),(271,''),(272,''),(273,'es funktioniert!\r\nUnglaublich!'),(274,'');
+INSERT INTO `text` VALUES (1,'Dienstag 16.2.2021'),(2,'Bestellung Tabletten für Oma'),(3,'Brot\r\nFisch \r\nGemüse'),(4,'Freitag 28.2.2021'),(5,'Lorem ipsum...'),(6,'Lorem ipsum...'),(7,'Simon ist toll'),(8,'Simon ist toll'),(9,'Simon ist toll'),(10,'Lorem iasdlgkjaölsdjfpsum...'),(11,'Lorem iasdlgkjaölsdjfpsum...'),(12,'asdf'),(13,'asdf'),(14,''),(15,''),(16,''),(17,'sg'),(18,''),(19,''),(20,'r'),(21,'c'),(22,''),(23,''),(24,''),(25,''),(26,''),(27,'asdf'),(28,'a'),(29,'ff'),(30,'fft'),(31,'fft'),(32,'g'),(33,''),(34,''),(35,''),(36,'f'),(37,'f'),(38,'fa'),(39,' fa '),(40,'  fa  '),(41,'   fa   '),(42,'    fa    '),(43,'     fa     '),(44,'      fa      '),(45,'       fa       '),(46,'        fa        '),(47,'         fa         '),(48,'          fa          '),(49,'fe'),(50,'fe'),(51,'fe'),(52,'nwtr'),(53,'nwtr'),(54,'saaa'),(55,'vv'),(56,''),(57,'headertest'),(58,'ffA'),(59,''),(60,'aafd'),(61,'tier'),(62,'wurst'),(63,'qq'),(64,'HET'),(65,''),(66,''),(67,''),(68,''),(69,''),(70,''),(71,''),(72,''),(73,''),(74,''),(75,''),(76,''),(77,''),(78,''),(79,''),(80,''),(81,''),(82,''),(83,''),(84,''),(85,''),(86,''),(87,''),(88,''),(89,''),(90,''),(91,''),(92,''),(93,''),(94,''),(95,''),(96,''),(97,''),(98,''),(99,''),(100,''),(101,''),(102,''),(103,''),(104,'grw'),(105,''),(106,''),(107,''),(108,''),(109,''),(110,''),(111,''),(112,''),(113,''),(114,''),(115,''),(116,''),(117,''),(118,''),(119,''),(120,''),(121,''),(122,''),(123,''),(124,''),(125,''),(126,''),(127,''),(128,''),(129,''),(130,''),(131,''),(132,''),(133,''),(134,''),(135,''),(136,''),(137,''),(138,''),(139,''),(140,''),(141,''),(142,''),(143,''),(144,''),(145,''),(146,''),(147,''),(148,''),(149,''),(150,''),(151,''),(152,''),(153,''),(154,''),(155,''),(156,''),(157,''),(158,''),(159,''),(160,''),(161,''),(162,''),(163,''),(164,'G$RW'),(165,''),(166,'RAA'),(167,'aww'),(168,'aebr'),(169,'65qw'),(170,'Ist ganz toll'),(171,''),(172,'mv '),(173,'mv '),(174,'mv '),(175,''),(176,''),(177,'WGVD'),(178,''),(179,''),(180,''),(181,''),(182,''),(183,''),(184,''),(185,'ff'),(186,'ff'),(187,'ff'),(188,'ff'),(189,'dA'),(190,'<br />\r\n<b>Warning</b>:  Undefined variable $text in <b>C:xampphtdocsgit\notredit.php</b> on line <b>52</b><br />\r\n'),(191,''),(192,'ASDF'),(193,'SGH'),(194,'SGH'),(195,'SGH'),(196,'SGH'),(197,'SGH'),(198,'SGH'),(199,'ADSF'),(200,'ADSF'),(201,'ADSF'),(202,'ADSF'),(203,'- Kurze Haare\r\n- Informatiker'),(204,''),(205,'asdf'),(206,''),(207,'asdf'),(208,''),(209,''),(210,'aegr'),(211,''),(212,''),(213,''),(214,''),(215,''),(216,'WEGF'),(217,'asdf'),(218,'aaa'),(219,'aaa'),(220,'awef'),(221,'aer'),(222,'Testtext1'),(223,'def'),(224,'aSFD'),(225,'ttt'),(226,'qaet'),(227,'z4'),(228,'aasdf'),(229,'asdf'),(230,'asdff'),(231,'adsf'),(232,'asdfasdfSE'),(233,'ooooo'),(234,'ooooo'),(235,'asdfasdfa'),(236,'rrrrr'),(237,'ppppp'),(238,'asdf'),(239,'asdf'),(240,'nein'),(241,'kkkkk'),(242,'FS'),(243,'WGE'),(244,'ffff'),(245,'ppp'),(246,'gg'),(247,'asdf'),(248,'ethat'),(249,'asdf'),(250,'asdf'),(251,''),(252,'asdf'),(253,'WGR'),(254,'RH<'),(255,'AGDFS'),(256,'fff'),(257,'ffff'),(258,'fff'),(259,'asdf'),(260,'dgdrgr'),(261,'asdf'),(262,'gR'),(263,'ggghtasdfasdasdf'),(264,'Butterhhh\r\nMehl'),(265,'Zucker\r\nMilch'),(266,'athreg'),(267,'fasdfasdfaaagfdtzksfdajd'),(268,'Schlimme Sache, das'),(269,'asdfasdfa'),(270,'asdfasdfasdfffff'),(271,''),(272,''),(273,'es funktioniert!\r\nUnglaublich!'),(274,'arwg'),(275,'sdfgsdfg'),(276,'fasdfasdf'),(277,'asdfa'),(278,'asdf'),(279,'asdf'),(280,'asdf'),(281,'aa'),(282,''),(283,'');
 /*!40000 ALTER TABLE `text` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -182,4 +182,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-03  0:26:12
+-- Dump completed on 2021-03-03 20:06:37
